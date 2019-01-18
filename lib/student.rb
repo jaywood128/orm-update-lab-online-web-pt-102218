0d32bdb52196b2a26bc87d05ac4c9253dc028a99
Johnathon Wood <jaywood128@gmail.com>
2019-01-18 19:34:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,9 @@ class Student
           FROM students
           WHERE name = ?
             SQL
-      DB[:conn].execute(sql, name)
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end
 
   end
 
