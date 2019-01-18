9677c44e359df3dcd24f000f8f2c26e35fb354e7
Johnathon Wood <jaywood128@gmail.com>
2019-01-18 18:49:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     sql = <<-SQL INSERT INTO students (name, grade)
             VALUES (?,?)
             SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-
+    DB[:conn].execute(sql, self.name, self.grade).map do |row|
+      self.new(row)
   end
 end
