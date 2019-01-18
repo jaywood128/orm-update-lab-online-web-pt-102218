d2e2856daa76bff7ee891a1f7324df2d69b98cd5
Johnathon Wood <jaywood128@gmail.com>
2019-01-18 19:21:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,11 +70,10 @@ class Student
     student.save
   end
   def self.new_from_db(row)
-    new_student = self.new 
+    new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
     new_student
-
   end
 end
