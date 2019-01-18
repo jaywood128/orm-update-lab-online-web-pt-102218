ce2f36eb2e9949d6286cef5ac61b265179cb94c8
Johnathon Wood <jaywood128@gmail.com>
2019-01-18 18:09:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,8 +25,7 @@ class Student
   def self.drop_table
 
     sql = <<-SQL
-      DROP TABLE IF EXISTS
-      WHERE name = students;
+      DROP TABLE IF EXISTS students;
 
       SQL
     DB[:conn].execute(sql)
